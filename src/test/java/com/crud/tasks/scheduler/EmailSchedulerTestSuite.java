1ee3773693a5be52edaed3a2d8a0dab86cf461c3
Arcadiusz <Arek94@onet.com.pl>
2018-09-29 18:14:11 +0200

    EmailSchedulerTest added to fill code coverage

---
+++ src/test/java/com/crud/tasks/scheduler/EmailSchedulerTestSuite.java
@@ -0,0 +1,42 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.matches;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailSchedulerTestSuite {
+
+    @InjectMocks
+    private EmailScheduler emailScheduler;
+
+    @Mock
+    private AdminConfig adminConfig;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private SimpleEmailService simpleEmailService;
+
+    @Test
+    public void shouldSendInformationEmail() {
+        //Given
+        //When
+        emailScheduler.sendInformationEmail();
+        //Then
+        verify(simpleEmailService, times(1)).send(any(Mail.class));
+    }
+}
